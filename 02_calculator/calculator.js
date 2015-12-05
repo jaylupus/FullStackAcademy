8ab078463a36c11ee75f1ea9c3c622118f5c4ea9
Jonah Wolf <wolfjonah@gmail.com>
2015-12-04 20:15:09 -0500

    Deleted duplicate files

--- 02_calculator/calculator.js
+++
@@ -1,8 +0,0 @@
-function createCalculator(){
-	var calc = {};
-	calc.val = 0;
-	calc.value = function(){return this.val};
-	calc.add = function(n){this.val += n};
-	calc.subtract = function(n){this.val -= n};
-	return calc;
-}
