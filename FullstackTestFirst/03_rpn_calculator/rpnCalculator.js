693009d255766c01c5937448a6c2faa15f46765b
Jonah Wolf <wolfjonah@gmail.com>
2015-12-09 11:14:44 -0500

    Incorporated Scott's suggestions into rpnCalculator

--- FullstackTestFirst/03_rpn_calculator/rpnCalculator.js
+++ FullstackTestFirst/03_rpn_calculator/rpnCalculator.js
@@ -2,42 +2,33 @@ var RPNCalculator = function(){
 	this.arr = [];
 };
 
+RPNCalculator.prototype.helper = function(func){
+	if (this.arr.length == 0){
+		throw "rpnCalculator is empty";
+	}
+	else{
+		var x = this.arr.pop();
+		var y = this.arr.pop();
+		this.arr.push(func(x, y));
+	}
+};
+
 RPNCalculator.prototype.value = function(){return this.arr[this.arr.length - 1]};
 
 RPNCalculator.prototype.push = function(n){this.arr.push(n)};
 	
 RPNCalculator.prototype.plus = function(){
-		if (this.arr.length == 0){
-			throw "rpnCalculator is empty";
-		}
-		else{
-			this.push(this.arr.pop() + this.arr.pop());
-		}
-	};
+	this.helper(function(a,b){return (a+b)});
+};
 	
 RPNCalculator.prototype.minus = function(){
-		if (this.arr.length == 0){
-			throw "rpnCalculator is empty";
-		}
-		else{
-			this.push(-(this.arr.pop() - this.arr.pop()));
-		}
-	};
+	this.helper(function(a,b){return (b - a)});
+};
 	
 RPNCalculator.prototype.times = function(){
-		if (this.arr.length == 0){
-			throw "rpnCalculator is empty";
-		}
-		else{
-			this.push(this.arr.pop() * this.arr.pop());
-		}
-	};
+	this.helper(function(a,b){return (a * b)});
+};
 	
 RPNCalculator.prototype.divide = function(){
-		if (this.arr.length == 0){
-			throw "rpnCalculator is empty";
-		}
-		else{
-			this.push((1 / this.arr.pop()) * this.arr.pop());
-		}
+	this.helper(function(a,b){return (b / a)});
 };
