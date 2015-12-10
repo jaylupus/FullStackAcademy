d193edfccf1350167d05a4a390d9e8fd7dcef721
Jonah Wolf <wolfjonah@gmail.com>
2015-12-10 14:24:31 -0500

    Added hint functionality

--- Guessing-Game/guessingGame.js
+++ Guessing-Game/guessingGame.js
@@ -12,14 +12,12 @@ var guesses = [];
 // Generate the Winning Number
 
 function generateWinningNumber(){
-	// add code here
 	return Math.floor(100 * Math.random());
 }
 
 // Fetch the Players Guess
 
 function playersGuessSubmission(){
-	// add code here
 	playersGuess = parseInt($("#guess").val());
 	$("#guess").attr("value", "");
 	checkGuess();
@@ -28,14 +26,12 @@ function playersGuessSubmission(){
 // Determine if the next guess should be a lower or higher number
 
 function lowerOrHigher(){
-	// add code here
 	return (playersGuess < winningNumber);
 }
 
 // Check if the Player's Guess is the winning number 
 
 function checkGuess(){
-	// add code here
 	if (playersGuess === winningNumber){
 		$("body").append("<p>You won!</p>");
 	}
@@ -47,13 +43,13 @@ function checkGuess(){
 
 function guessMessage(){
 	if (guesses.indexOf(playersGuess) != -1){
-		return "You already guessed that!";
+		return "You already guessed that! Try a new number.";
 	}
 	else if (lowerOrHigher()){
-		return "Too low!";
+		return "Too low! Guess again.";
 	}
 	else{
-		return "Too high!";
+		return "Too high! Guess again.";
 	}
 
 }
@@ -61,8 +57,38 @@ function guessMessage(){
 // Create a provide hint button that provides additional clues to the "Player"
 
 function provideHint(){
-	// add code here
+	var remainingGuesses = (5 - guesses.length);
+	var numHints = 2 * remainingGuesses;
+	var hints = [];
+	hints.push(winningNumber);
+	while (numHints > 1){
+		hints.push(generateWinningNumber());
+		numHints--;
+	}
+	shuffle(hints);
+	$("form").append("<p>The winning number is one of the following: " + hints + ".</p>");
 }
+
+//Copied this from internet!
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 
 // Allow the "Player" to Play Again
 
@@ -72,4 +98,5 @@ function playAgain(){
 
 
 /* **** Event Listeners/Handlers ****  */
-$("button").click(playersGuessSubmission);
+$("#submit").click(playersGuessSubmission);
+$("#hint").click(provideHint);
